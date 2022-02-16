8ed00332a4531293cb2e444676c6da087652c198
Ice <Ice@DESKTOP-RQ35T8H>
2022-02-16 21:38:06 +0800

    first commit

--- src/comp3021/HelloWorld.java
+++ src/comp3021/HelloWorld.java
@@ -2,6 +2,8 @@ package comp3021;
 
 public class HelloWorld {
 	
+		//Main function
+		//print our hello world
 		public static void main(String[] args) {
 			System.out.println("Hello World");
 		}
